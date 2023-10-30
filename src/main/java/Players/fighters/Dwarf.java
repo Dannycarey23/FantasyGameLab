36644b5af1dc8efebc20df325976ea700fb5c88f
Danny <carey.d@ntlworld.com>
2023-10-30 22:27:11 +0000

    Added dwarf class

--- src/main/java/Players/fighters/Dwarf.java
+++ src/main/java/Players/fighters/Dwarf.java
@@ -1,4 +1,24 @@
 package Players.fighters;
 
-public class Dwarf {
+import Interfaces.IWeapon;
+
+public class Dwarf extends Fighter{
+    private IWeapon weapon;
+
+    public Dwarf(String name, int hp, IWeapon weapon) {
+        super(name, hp);
+        this.weapon = weapon;
+    }
+
+    public IWeapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(IWeapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public String catchPhrase(){
+        return "Nobody tosses a dwarf!";
+    }
 }
