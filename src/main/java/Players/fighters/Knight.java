447ab6638c1d0e1d3f2c924bd844eec1cd91a2b5
Danny <carey.d@ntlworld.com>
2023-10-30 22:28:14 +0000

    added Knight class

--- src/main/java/Players/fighters/Knight.java
+++ src/main/java/Players/fighters/Knight.java
@@ -1,4 +1,24 @@
 package Players.fighters;
 
-public class Knight {
+import Interfaces.IWeapon;
+
+public class Knight extends Fighter{
+    private IWeapon weapon;
+
+    public Knight(String name, int hp, IWeapon weapon) {
+        super(name, hp);
+        this.weapon = weapon;
+    }
+
+    public IWeapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(IWeapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public String catchPhrase(){
+        return "It's just a flesh wound";
+    }
 }
