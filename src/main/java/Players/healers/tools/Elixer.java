bd5287dcb2c63119d9b878a14739ded182ee2e49
Danny <carey.d@ntlworld.com>
2023-10-30 22:57:40 +0000

    added healing tools that implement IHeal interface

---
+++ src/main/java/Players/healers/tools/Elixer.java
@@ -0,0 +1,10 @@
+package Players.healers.tools;
+
+import Interfaces.IHeal;
+
+public class Elixer implements IHeal {
+    @Override
+    public int heal() {
+        return 0;
+    }
+}
---
+++ src/main/java/Players/healers/tools/Potion.java
@@ -0,0 +1,10 @@
+package Players.healers.tools;
+
+import Interfaces.IHeal;
+
+public class Potion implements IHeal {
+    @Override
+    public int heal() {
+        return 0;
+    }
+}
