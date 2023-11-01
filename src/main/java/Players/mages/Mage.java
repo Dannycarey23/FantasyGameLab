885901ada2c363b026786030cf968e5f7be4bf13
Danny <carey.d@ntlworld.com>
2023-11-01 14:49:12 +0000

    updated Mage class tp include IDefend and ISpell in the constructor, included getters and setters

--- src/main/java/Players/mages/Mage.java
+++ src/main/java/Players/mages/Mage.java
@@ -4,8 +4,28 @@ import Interfaces.IDefend;
 import Interfaces.ISpell;
 import Players.Player;
 
-public abstract class Mage extends Player implements ISpell, IDefend{
-    public Mage(String name, int hp) {
+public abstract class Mage extends Player {
+    private IDefend beast;
+    private ISpell spell;
+    public Mage(String name, int hp, IDefend beast, ISpell spell) {
         super(name, hp);
+        this.beast = beast;
+        this.spell = spell;
+    }
+
+    public IDefend getBeast() {
+        return beast;
+    }
+
+    public void setBeast(IDefend beast) {
+        this.beast = beast;
+    }
+
+    public ISpell getSpell() {
+        return spell;
+    }
+
+    public void setSpell(ISpell spell) {
+        this.spell = spell;
     }
 }
